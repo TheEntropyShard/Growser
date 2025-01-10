26970f37258513a502747b4615da931b51fa3ac6
TheEntropyShard <theentropyshard@gmail.com>
2025-01-10 23:31:42 +0300

    Updated copyright year

--- src/main/java/me/theentropyshard/growser/gui/text/GemtextViewFactory.java
+++ src/main/java/me/theentropyshard/growser/gui/text/GemtextViewFactory.java
@@ -1,6 +1,6 @@
 /*
  * Growser - https://github.com/TheEntropyShard/Growser
- * Copyright (C) 2023-2024 TheEntropyShard
+ * Copyright (C) 2023-2025 TheEntropyShard
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
